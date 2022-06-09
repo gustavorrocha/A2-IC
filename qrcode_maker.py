1f30999f84b48e259402dc5fd1833716c344c797
Superalien10 <105555970+Superalien10@users.noreply.github.com>
2022-06-09 00:50:18 -0300

    Atualize qrcode_maker.py
    Aproveite a estrutura de carteira já desenvolvida em outro branch(DataExcel).

--- qrcode_maker.py
+++ qrcode_maker.py
@@ -1,20 +1,23 @@
 import qrcode
 #Esse módulo é necessário para criar um qrcode e associar algo a ele.
-carteira_1 = {}
-carteira_2 = {}
-carteira_3 = {}
-#Essas carteiras são modelos para testar o programa.
-valores_hoje = {}
-#Esse dicionário aramzenará o valor de cada moeda e ação.
-total_carteira = 0
+
+soma = 0
 #Essa variável receberá o valor da carteira analisada.
 
-for tipo, quantidade in carteira_1:
-    valor = valores_hoje[tipo]
-    total_carteira = total_carteira + quantidade*valor
-#Essa estrutura percorre a carteira_1, e soma ao total da carteira o valor de cada componente da carteira.
+carteira = {'Ações': [{'Nome': 'GOOGL', 'Quantidade': 2, 'Preço (unitário)': 11451.03, 'Preço (total)': 22902.06}, {'Nome': 'AMZN', 'Quantidade': 1, 'Preço (unitário)': 592.02, 'Preço (total)':592.02}, {'Nome': 'AAPL', 'Quantidade': 5, 'Preço (unitário)': 722.86, 'Preço (total)': 3614.3}, {'Nome': 'PETR4.SA', 'Quantidade': 10, 'Preço (unitário)': 149.06, 'Preço (total)': 1490.6}, {'Nome': 'VALE', 'Quantidade': 15, 'Preço (unitário)': 87.4, 'Preço (total)': 1311.0}], 'Moedas': [{'Nome': 'USD', 'Quantidade': 150, 'Preço (unitário)': 4.89, 'Preço (total)': 733.5}, {'Nome': 'BRL', 'Quantidade': 200, 'Preço (unitário)': 1.0, 'Preço (total)': 200.0}, {'Nome': 'EUR', 'Quantidade': 100, 'Preço (unitário)': 5.24, 'Preço (total)': 524.0}]}
+#Essa carteira é um modelo para testar o programa.
+
+acoes = carteira['Ações']
+for acao in acoes:
+    soma = soma + acao['Preço (total)']
+#Essa estrutura percorre cada elemento da tupla armazenada em acoes. Esse elemento é um dicionário, do qual seleciona-se o valor correspondente à chave 'Preço (total)'. Esse valor é somado à variável soma.
     
-imagem_qrcode = qrcode.make(f"Você tem o equivalente a {total_carteira} reais.")
-#Esse comando cria um qrcode associado à frase inserida como parÂMETRO.
-imagem_qrcode.save("qrcode_python.png")
+moedas = carteira['Moedas']
+for moeda in moedas:
+    soma = soma + moeda['Preço (total)']
+#Essa estrutura percorre cada elemento da tupla armazenada em moedas. Esse elemento é um dicionário, do qual seleciona-se o valor correspondente à chave 'Preço (total)'. Esse valor é somado à variável soma.
+
+imagem_qrcode = qrcode.make(f"Você tem o equivalente a {soma} reais.")
+#Esse comando cria um qrcode associado à frase inserida como parâmetro.
+imagem_qrcode.save("carteira.png")
 #Esse comando salva o qrcode como um arquivo png.
