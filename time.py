f93a74814f6a195b1226bbb29628e2680b2e4424
Superalien10 <105555970+Superalien10@users.noreply.github.com>
2022-06-16 00:29:18 -0300

    Atualize time.py
    O código desse arquivo consulta dados do yfinance. Esses dados serão usados em charts.py para criar gráficos.

--- time.py
+++ time.py
@@ -1,1 +1,29 @@
+#O código a seguir coleta as cotações das ações nos últimos meses.
 
+from datetime import date, timedelta
+import yfinance as yf
+from datetime import datetime
+import pandas
+
+hoje = date.today()
+passado = hoje - timedelta(365)
+
+ativos = {"Ambev":"ABEV3.SA", "Bancodobrasil":"BBAS3.SA", "B3":"B3SA3.SA", "Cielo":"CIEL3.SA", "Eletrobras":"ELET3.SA", "Fleury":"FLRY3.SA", "Gol":"GOLL4.SA", "Jbs":"JBSS3.SA", "Mrv":"MRVE3.SA", "Petrobras":"PETR3.SA", "Vale":"VALE3.SA"}
+for companhia, codigo in ativos.items():
+    nome = companhia
+    x = yf.Ticker(codigo).history(start = passado, end = hoje, interval = "1mo")
+    print(nome, ": \n", x, "\n")
+    contador = -1
+    lista = []
+    tam = len(x.index)
+    index = range(tam)
+    x.index = index
+    for linha in x.values:
+        contador += 1    
+        for celula in linha:
+            if pandas.isna(celula) == True:
+                lista.append(contador)          
+    x = pandas.DataFrame(data=x.drop(lista))
+    companhia = x      
+    print(nome, ": \n", companhia, "\n")
+    
