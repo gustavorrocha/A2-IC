96375e78a9ee0960a2b12d3ee6725c80c3f15917
Rodrigo Prieto <89661120+rdhery@users.noreply.github.com>
2022-06-17 14:57:57 -0300

    Altere o arquivo webscraping.py

--- webscraping.py
+++ webscraping.py
@@ -1,15 +1,30 @@
+# Importando as bibliotecas a serem usadas.
 import requests
 import lxml
 from bs4 import BeautifulSoup
 
-url = "https://almirfonseca.github.io/A2-IC/carteira_mariana.html"
-response = requests.get(url)
+# Criando a função para fazer o webscraping.
+def webscraping(url):
+    # Retirando o html da carteira.
+    response = requests.get(url)
+    # Facilitando a compreensão do html.
+    pagina = BeautifulSoup(response.text, 'lxml')
 
-page = BeautifulSoup(response.text, 'lxml')
-dict = {}
-for actions in page.select("div tr"):
-    action = actions.select("td")
-    if len(action) > 0:
-        dict[actions.select("td")[0].text] = actions.select("td")[1].text
+    # Criando dicionários para receber os dados da carteira, especificando se é ação
+    # ou moeda.
+    carteira = {"Ações":[], "Moedas":[]}
+    acoes = {}
+    moedas = {}
 
-print(dict)
+    # Retirando os dados das ações e das moedas da carteira.
+    for acao in pagina.select(".acao tr"):
+        action = acao.select("td")
+        if len(action) > 0:
+            acoes[acao.select("td")[0].text] = acao.select("td")[1].text
+    carteira["Ações"].append(acoes)
+    for moeda in pagina.select(".moeda tr"):
+        action = moeda.select("td")
+        if len(action) > 0:
+            moedas[moeda.select("td")[0].text] = moeda.select("td")[1].text
+    carteira["Moedas"].append(moedas)
+    return carteira
