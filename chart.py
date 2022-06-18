3f691779853f5b5569a2d778cb3da2251627069f
Superalien10 <105555970+Superalien10@users.noreply.github.com>
2022-06-18 06:16:59 -0300

    Atualize chart.py
    Crie uma função que prepará os dados do terceiro gráfico. Adicione as importações necessárias. Confeccione o terceiro gráfico.

--- chart.py
+++ chart.py
@@ -1,7 +1,29 @@
 #Nesse arquivo serão gerados gráficos a partir dos dados das carteiras.
 
 from openpyxl import load_workbook #Essa função será necessária para carregar o workbook gerado anteriormente (no branch DadosExcel).
-from openpyxl.chart import BarChart, Series, Reference #Essas funções são necessárias para construir os gráficos.
+from openpyxl.chart import BarChart, Series, Reference, LineChart #Essas funções são necessárias para construir os gráficos.
+from datetime import date, timedelta
+import yfinance as yf
+from datetime import datetime
+import pandas
+
+def gerar(companhia, codigo):
+    hoje = date.today()
+    passado = hoje - timedelta(365)
+    nome = companhia
+    y = yf.Ticker(codigo).history(start = passado, end = hoje, interval = "1mo")
+    contador = -1
+    lista = []
+    tam = len(y.index)
+    index = range(tam)
+    y.index = index
+    for linha in y.values:
+        contador += 1    
+        for celula in linha:
+            if pandas.isna(celula) == True:
+                lista.append(contador)
+    x = pandas.DataFrame(data=y.drop(lista))  
+    return x
 
 wb = load_workbook(filename = 'teste189.xlsx') #Carrega o arquivo em excel.
 ws = wb.active #Grava a planilha ativa.
@@ -53,6 +75,61 @@ data = Reference(ws_2, min_col=1, min_row=1, max_row=2, max_col=8)
 chart2.add_data(data, titles_from_data=True)
 chart2.shape = 4
 ws.add_chart(chart2, "I18")
+
+#Vamos fazer um gráfico de linhas
+
+cabecalho = []
+m1 = []
+m2 = []
+m3 = []
+m4 = []
+m5 = []
+m6 = []
+m7 = []
+m8 = []
+m9 = []
+m10 = []
+m11 = []
+m12 = []
+m13 = []
+ativos = {"Ambev":"ABEV3.SA", "Bancodobrasil":"BBAS3.SA", "B3":"B3SA3.SA", "Cielo":"CIEL3.SA", "Eletrobras":"ELET3.SA", "Fleury":"FLRY3.SA", "Gol":"GOLL4.SA", "Jbs":"JBSS3.SA", "Mrv":"MRVE3.SA", "Petrobras":"PETR3.SA", "Vale":"VALE3.SA"}
+for companhia, codigo in ativos.items():
+    x = gerar(companhia, codigo)
+    
+    cabecalho.append(companhia)
+    m1.append(x.iloc[0,0])
+    m2.append(x.iloc[1,0])
+    m3.append(x.iloc[2,0])
+    m4.append(x.iloc[3,0])
+    m5.append(x.iloc[4,0])
+    m6.append(x.iloc[5,0])
+    m7.append(x.iloc[6,0])
+    m8.append(x.iloc[7,0])
+    m9.append(x.iloc[8,0])
+    m10.append(x.iloc[9,0])
+    m11.append(x.iloc[10,0])
+    m12.append(x.iloc[11,0])
+    m13.append(x.iloc[12,0])
+
+linhas = (["","",""], cabecalho, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13)
+print(linhas)
+for novo in linhas:
+    ws_2.append(novo)
+c1 = LineChart()
+
+c1.width = 13
+c1.height = 8
+c1.title = "Evolução do valor dos ativos"
+c1.style = 13
+c1.y_axis.title = 'Valor'
+c1.x_axis.title = 'Tempo'
+
+tanto = len(ativos)
+data = Reference(ws, min_col=1, min_row=1, max_col=tanto, max_row=14)
+c1.add_data(data, titles_from_data=True)
+
+ws.add_chart(c1, "A15")
+
 
 #Uma cópia do arquivo com as modificações feitas é criada.
 wb.save("bar.xlsx")
